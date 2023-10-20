f792972bc6109509e5416dfaafb6a6a51626c5aa
NICOL1201 <117902091+NICOL120@users.noreply.github.com>
2023-10-20 23:43:13 +0800

    Update PrimarySlice.tsx

--- apps/enterprise/src/pages/landing/PrimarySlice.tsx
+++ apps/enterprise/src/pages/landing/PrimarySlice.tsx
@@ -3,7 +3,7 @@ import { Text } from 'lib/ui/Text';
 import { useDisplay } from 'hooks';
 import { useNavigate } from 'react-router';
 import styles from './PrimarySlice.module.sass';
-import ButtonContainer from './ButtonContainer'; // Menggunakan ButtonContainer dengan benar
+import ButtonContainer from './ButtonContainer';
 
 export const PrimarySlice = () => {
   const navigate = useNavigate();
