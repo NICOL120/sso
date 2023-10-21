a34c055fbb9b8f83b3cc91bd55da10e34463eb8d
NICOL1201 <117902091+NICOL120@users.noreply.github.com>
2023-10-22 00:50:24 +0800

    Update TopDAOs.tsx

--- apps/enterprise/src/pages/dashboard/TopDAOs.tsx
+++ apps/enterprise/src/pages/dashboard/TopDAOs.tsx
@@ -2,7 +2,6 @@ import { useAllDaosQuery } from 'dao/hooks/useAllDaosQuery';
 import { LabeledPageSection } from 'lib/ui/LabeledPageSection';
 import { SameWidthChildrenRow } from 'lib/ui/Layout/SameWidthChildrenRow';
 import { DAOCard } from 'pages/shared/DAOCard';
-import styles from './Topdao.module.css';
 
 export const RecentDAOs = () => {
   const { data = [] } = useAllDaosQuery();
@@ -10,12 +9,12 @@ export const RecentDAOs = () => {
   const daos = data.sort((a, b) => (b.tvl ?? 0) - (a.tvl ?? 0)).slice(0, 6);
 
   return (
-    <div className={classNames(styles.btn)}>
     <LabeledPageSection name="">
+      <SameWidthChildrenRow maxColumns={3} fullWidth minChildrenWidth={320} gap={16}>
         {daos.map((dao, index) => (
           <DAOCard key={index} dao={dao} />
         ))}
+      </SameWidthChildrenRow>
     </LabeledPageSection>
-    </div>
   );
 };
